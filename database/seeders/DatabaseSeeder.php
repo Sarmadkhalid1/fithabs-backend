e352eea50c17d5baa1a6479c77f16a3f13d16629
Sarmadkhalid1 <sarmadkhalid36@gmail.com>
2025-09-07 03:05:27 +0500

    seeder error fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\AdminUserSeeder;
 use Database\Seeders\WorkoutSeeder; 
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         $this->call([
+            AdminUserSeeder::class,
             WorkoutSeeder::class,
             ExerciseSeeder::class,
             VideoSeeder::class,
